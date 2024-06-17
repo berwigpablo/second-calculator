9c543433de157d940c1c838aaad246b8d9dd35f1
Pablo <104324647+berwigpablo@users.noreply.github.com>
2024-06-17 18:56:54 -0300

    Begin script build

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
 const numbers = Array.from(document.querySelector('.numbers').children);
-const operators = document.querySelector('.operators').children;
+const operators = Array.from(document.querySelector('.operators').children);
+const display = document.querySelector('.displayText');
+let firstString = '';
+let secondString = '';
+
 
 numbers.forEach(number => number.addEventListener('mousedown', buttonClick));
-numbers.forEach(number => number.addEventListener('click', buttonClick));
-
+numbers.forEach(number => number.addEventListener('mouseup', buttonClick));
+operators.forEach(operator => operator.addEventListener('mousedown', buttonClick));
+operators.forEach(operator => operator.addEventListener('mouseup', buttonClick));
 
 function buttonClick(event){
-    let button = event.target;
-    button.classList.toggle('clicked');
+    let char = event.target;
+    char.classList.toggle('clicked');
+
+    if(event.type === "mouseup"){
+        updateDisplay(char);
+    };
+}
+
+function updateDisplay(button){
+    char = button.textContent;
+    firstString += char;
+
+    display.textContent += char;
+    console.log(firstString);
 }
