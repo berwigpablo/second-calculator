13a582af33d89e1d0400bacda73ef92b81d94640
Pablo <104324647+berwigpablo@users.noreply.github.com>
2024-06-18 09:50:45 -0300

    Add clear button

--- script.js
+++ script.js
@@ -1,25 +1,45 @@
 const numbers = Array.from(document.querySelector('.numbers').children);
 const operators = Array.from(document.querySelector('.operators').children);
 const display = document.querySelector('.displayText');
+const clear = document.querySelector('.clear');
 let firstString = '';
 let secondString = '';
-
 
 numbers.forEach(number => number.addEventListener('mousedown', buttonClick));
 numbers.forEach(number => number.addEventListener('mouseup', buttonClick));
 operators.forEach(operator => operator.addEventListener('mousedown', buttonClick));
 operators.forEach(operator => operator.addEventListener('mouseup', buttonClick));
+document.addEventListener('keydown', checkButton);
+
+function clearDisplay(){
+    display.textContent = '';
+    char = '';
+    firstString = '';
+    secondString = '';
+}
+
+function checkButton(event){
+    buttonClick(event)
+}
 
 function buttonClick(event){
-    let char = event.target;
-    char.classList.toggle('clicked');
+    console.log(event);
 
-    if(event.type === "mouseup"){
-        updateDisplay(char);
-    };
+    if(event.target.classList.value === 'clear' || event.key === "Escape"){
+        clearDisplay()
+
+    } else{
+        let char = event.target;
+        char.classList.toggle('clicked');
+
+        if(event.type === "mouseup"){
+            updateDisplay(char);
+        };
+    }
 }
 
 function updateDisplay(button){
+    console.log(button);
     char = button.textContent;
     firstString += char;
 
